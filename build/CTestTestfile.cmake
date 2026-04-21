7ff87e39b3175897bf513921cbcc525955fa4c1c
ammar869 <ammarhassan1131318@gmail.com>
2026-04-22 02:32:23 +0500

    Remove-Item -Recurse -Force build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ammar/Documents/Kernal
-# Build directory: C:/Users/Ammar/Documents/Kernal/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ammar/Documents/Kernal/tests
-# Build directory: C:/Users/Ammar/Documents/Kernal/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(IntegrationTests "C:/Users/Ammar/Documents/Kernal/build/bin/MiniOSIntegrationTests.exe")
-set_tests_properties(IntegrationTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Ammar/Documents/Kernal/tests/CMakeLists.txt;18;add_test;C:/Users/Ammar/Documents/Kernal/tests/CMakeLists.txt;0;")
