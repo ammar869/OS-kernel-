6134afe773cc91ff99f2d272bd0720dc648161d8
ammar869 <ammarhassan1131318@gmail.com>
2026-04-22 02:23:35 +0500

    Ninja setup

--- build/tests/CMakeFiles/MiniOSIntegrationTests.dir/MiniOSIntegrationTests_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-tests/CMakeFiles/MiniOSIntegrationTests.dir/MiniOSIntegrationTests_autogen/mocs_compilation.cpp.obj: \
- C:\Users\Ammar\Documents\Kernal\build\tests\MiniOSIntegrationTests_autogen\mocs_compilation.cpp
